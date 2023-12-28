8f9297b0566aad314141c1ee4c465078ea8f71aa
Koushik Saha <60405003+koushik0005@users.noreply.github.com>
2023-12-29 00:32:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let inputBox = document.querySelector('#inputBox')
+let listContainer = document.querySelector('.list-container')
+
+function addTesk(){
+    if(inputBox.value === ''){
+        alert('Your must be input something')
+    }else{
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value
+        listContainer.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+
+    }
+    inputBox.value = ''
+    saveData()
+}
+
+inputBox.addEventListener('keyup', (e)=>{
+    if(e.keyCode === 13){
+        if(inputBox.value === ''){
+            alert('Your must be input something')
+        }else{
+            let li = document.createElement('li')
+            li.innerHTML = inputBox.value
+            listContainer.appendChild(li)
+
+            let span = document.createElement("span")
+            span.innerHTML = "\u00d7"
+            li.appendChild(span)
+        }
+        inputBox.value = ''
+        saveData()
+    }
+})
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+
+function showtask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+showtask()
